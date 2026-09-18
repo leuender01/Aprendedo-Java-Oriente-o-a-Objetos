1db95ac33bde0449f0b6baee0e15df5018e3e784
leuender <leuenderoliveirabena@gmail.com>
2026-09-18 00:58:48 -0300

    ADD Funcoes.java, Variaveis.java

---
+++ aprendendo_java/Funcoes.java
@@ -0,0 +1,12 @@
+public class Funcoes {
+    public static void main(String[] arg){
+        System.out.println(sum(20, 20));
+        int resultado = sum(20, 20);
+        System.out.println(resultado);
+    }
+    
+    static int sum(int i, int x){
+        return i + x;
+    }
+    //Syntax <valor de retorno > <nome da função> (<parametros>) { <corpo da função}>
+}
---
+++ aprendendo_java/Variaveis.java
@@ -0,0 +1,20 @@
+public class Variaveis{
+    public static void main(String[] arg){
+        System.out.println("-----Variaveis-----");
+        // Declaração de variáveis Dinamicas
+//        var login = "Ola Mundo!";
+//        var idade = 30;
+        //variaveis -> tipo: Texto (String)
+        //variaveis -> tipo: Numeros (Int)
+        //variaveis -> tipo: Nuemros Francionados (Double)
+        //variaveis -> tipo: booleanos (boolean)
+        String nome = "Leu do rodo";
+        int idade2 = 30;
+        double altura = 1.75;
+        boolean ativo = true;
+        System.out.println(nome);
+        System.out.println(idade2);
+        System.out.println(altura);
+        System.out.println(ativo);
+    }
+}
