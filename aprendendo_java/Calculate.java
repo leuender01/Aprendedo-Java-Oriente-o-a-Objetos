1e696cb84ec4687c600b8b9c9d5280e5b11b3333
leuender <leuenderoliveirabena@gmail.com>
2026-09-18 16:22:51 -0300

    aprendendo funcoes java

---
+++ aprendendo_java/Calculate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Calculate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String texto = sc.nextLine();
+        System.out.println("Texto: " + texto);
+        sc.close();
+    }
+}
--- aprendendo_java/Funcoes.java
+++ aprendendo_java/Funcoes.java
@@ -1,12 +1,19 @@
+import java.util.Random;
+
 public class Funcoes {
-    public static void main(String[] arg){
-        System.out.println(sum(20, 20));
-        int resultado = sum(20, 20);
-        System.out.println(resultado);
+    public static void main(String[] args){
+        Random number = new Random();
+        int num1 = number.nextInt(100);
+        int num2 = number.nextInt(100);
+        System.out.println(sum(num1, num2));
+        for(int i = 0; i < args.length ; i++){
+            System.out.println(args[i]);
+        }
     }
     
-    static int sum(int i, int x){
+    static double sum(double i, double x){
         return i + x;
     }
+    
     //Syntax <valor de retorno > <nome da função> (<parametros>) { <corpo da função}>
 }
